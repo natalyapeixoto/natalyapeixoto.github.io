8917a2956a2662c56746024dce2dc6237183001a
Natalya <natalya_peixoto@hotmail.com>
2018-10-17 16:29:04 -0300

    form validation and arrow to the top

--- script/index.js
+++ script/index.js
@@ -2,6 +2,7 @@ let form = $(".form-contato");
 console.log(form);
 
 form.validate({
+  errorElement: 'span',
   rules: {
     email: {
         required: true,
@@ -9,5 +10,28 @@ form.validate({
     },
     tel: 'required',
     text: 'required'
-  }
+  }, 
+  messages: {
+    email: {
+      required: "Por favor, digite seu e-mail.",
+      email: "Por favor, digite um e-mail válido"
+      }, 
+    tel: 'Por favor, digite seu número de telefone',
+  text: 'Me manda uma pequena mensagem'
+    }
+})
+
+
+const button = document.getElementById("arrowTop")
+
+button.addEventListener("click", function(){
+    window.scrollTo(pageYOffset, 0);
+})
+
+window.addEventListener("scroll", function(){
+    if(pageYOffset > 450){
+        button.hidden = false;
+        return false;
+    }
+    button.hidden = true;
 })
